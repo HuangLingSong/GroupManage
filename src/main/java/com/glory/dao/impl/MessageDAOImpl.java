43816808a34d6185ec71bdfe802750fe9da947bb
zhangyanli <467627195@qq.com>
2017-11-27 21:39:12 +0800

    mmm

---
+++ src/main/java/com/glory/dao/impl/MessageDAOImpl.java
@@ -0,0 +1,29 @@
+package com.glory.dao.impl;
+
+import com.glory.dao.MessageDAO;
+import com.glory.entity.Message;
+import com.glory.libraries.DBManager;
+
+/**
+ * Created by Monster on 2017/11/18.
+ */
+public class MessageDAOImpl implements MessageDAO {
+
+    DBManager dbManager = null;
+//添加信息
+    @Override
+    public int forgotPasswd(Message message) {
+        dbManager = new DBManager();
+        int resultSet = 0;
+        try {
+			//sql语句
+            String sql = "insert into t_message (content) value (?)";
+            resultSet = dbManager.doUpdate(sql, new Object[]{message.getContent()});
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dbManager.CloseConnection();
+        }
+        return resultSet;
+    }
+}
